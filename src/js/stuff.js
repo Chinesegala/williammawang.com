f926934eea3141bcc3412e2c1f35dc90c2f566d9
Chinesegala <Willwang.org@outlook.com>
2023-03-24 17:38:16 -0500

    seo

--- src/js/stuff.js
+++ src/js/stuff.js
@@ -15,46 +15,23 @@ $(document).ready(function () {
   });
 });
 
-let freeAnimationTriggered = false;
-
-// add animate__fadeIn class to #free-sect element
-document.querySelector('#free-sect').classList.add('animate__fadeIn');
-
-window.addEventListener('scroll', () => {
-  if (window.scrollY < 500 && !freeAnimationTriggered) {
-     // remove animate__fadeOut class from #free-sect element
-     document.querySelector('#free-sect').classList.remove('animate__fadeOut');
-     // add animate__fadeIn class to #free-sect element
-     document.querySelector('#free-sect').classList.add('animate__fadeIn');
-     freeAnimationTriggered = true;
-  } else if (window.scrollY >= 500 && freeAnimationTriggered) {
-     // remove animate__fadeIn class from #free-sect element
-     document.querySelector('#free-sect').classList.remove('animate__fadeIn');
-     // add animate__fadeOut class to #free-sect element
-     document.querySelector('#free-sect').classList.add('animate__fadeOut');
-     freeAnimationTriggered = false;
-  }
-});
-
-// add invisible class to element by default
-$('#blog-sect').addClass('invisible');
-
 let blogAnimationTriggered = false;
 
+// add animate__fadeIn class to #free-sect element
+document.querySelector('#blog-sect').classList.add('animate__fadeIn');
+
 window.addEventListener('scroll', () => {
-  if (window.scrollY > 300 && !blogAnimationTriggered) {
-     // remove invisible class from #bio element
-     document.querySelector('#blog-sect').classList.remove('invisible');
-     // add animate__jackInTheBox class to #bio element
+  if (window.scrollY < 500 && !blogAnimationTriggered) {
+     // remove animate__fadeOut class from #free-sect element
+     document.querySelector('#blog-sect').classList.remove('animate__fadeOut');
+     // add animate__fadeIn class to #free-sect element
      document.querySelector('#blog-sect').classList.add('animate__fadeIn');
-     // set animationTriggered to true
      blogAnimationTriggered = true;
-  } else if (window.scrollY <= 300 && blogAnimationTriggered) {
-     // remove animate__jackInTheBox class from #bio element
+  } else if (window.scrollY >= 500 && blogAnimationTriggered) {
+     // remove animate__fadeIn class from #free-sect element
      document.querySelector('#blog-sect').classList.remove('animate__fadeIn');
-     // add another animation class to #bio element
-     document.querySelector('#blog-sect').classList.add('animate__backOutDown');
-     // set animationTriggered to false
+     // add animate__fadeOut class to #free-sect element
+     document.querySelector('#blog-sect').classList.add('animate__fadeOut');
      blogAnimationTriggered = false;
   }
 });
@@ -65,20 +42,43 @@ $('#psych-sect').addClass('invisible');
 let psychAnimationTriggered = false;
 
 window.addEventListener('scroll', () => {
-  if (window.scrollY > 900 && !psychAnimationTriggered) {
+  if (window.scrollY > 300 && !psychAnimationTriggered) {
      // remove invisible class from #bio element
      document.querySelector('#psych-sect').classList.remove('invisible');
      // add animate__jackInTheBox class to #bio element
      document.querySelector('#psych-sect').classList.add('animate__fadeIn');
      // set animationTriggered to true
      psychAnimationTriggered = true;
-  } else if (window.scrollY <= 900 && psychAnimationTriggered) {
+  } else if (window.scrollY <= 300 && psychAnimationTriggered) {
      // remove animate__jackInTheBox class from #bio element
      document.querySelector('#psych-sect').classList.remove('animate__fadeIn');
      // add another animation class to #bio element
      document.querySelector('#psych-sect').classList.add('animate__backOutDown');
      // set animationTriggered to false
      psychAnimationTriggered = false;
+  }
+});
+
+// add invisible class to element by default
+$('#free-sect').addClass('invisible');
+
+let freeAnimationTriggered = false;
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 900 && !freeAnimationTriggered) {
+     // remove invisible class from #bio element
+     document.querySelector('#free-sect').classList.remove('invisible');
+     // add animate__jackInTheBox class to #bio element
+     document.querySelector('#free-sect').classList.add('animate__fadeIn');
+     // set animationTriggered to true
+     freeAnimationTriggered = true;
+  } else if (window.scrollY <= 900 && freeAnimationTriggered) {
+     // remove animate__jackInTheBox class from #bio element
+     document.querySelector('#free-sect').classList.remove('animate__fadeIn');
+     // add another animation class to #bio element
+     document.querySelector('#free-sect').classList.add('animate__backOutDown');
+     // set animationTriggered to false
+     freeAnimationTriggered = false;
   }
 });
 
